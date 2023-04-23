366ac05a1d875c533ed4d42ea569b0e2cb827508
Aleksa Milenovic <73794793+mileni98@users.noreply.github.com>
2023-04-24 01:53:00 +0200

    fix: completed etl for the first DW theme

--- Relational databases/SRCBoking_InsertData.sql
+++ Relational databases/SRCBoking_InsertData.sql
@@ -1,21 +1,21 @@
 /*==============================================================*/
 /* Table: SRC_Country                                           */
 /*==============================================================*/
-INSERT INTO SRC_Country (country_id, country_name) VALUES ();
+INSERT INTO "SSP_RES".SRC_Country (country_id, country_name) VALUES (1, "Italy");
 commit;
 
 
 /*==============================================================*/
 /* Table: SRC_City                                              */
 /*==============================================================*/
-INSERT INTO CREATE SRC_City (city_id, city_name, country_id) VALUES ();
+INSERT INTO "SSP_RES".SRC_City (city_id, city_name, country_id) VALUES ();
 commit;
 
 
 /*==============================================================*/
 /* Table: SRC_Guest                                             */
 /*==============================================================*/
-INSERT INTO SRC_Guest (guest_id, guest_firstname, guest_lastname, guest_password, guest_email, guest_contact, country_id)
+INSERT INTO "SSP_RES".SRC_Guest (guest_id, guest_firstname, guest_lastname, guest_password, guest_email, guest_contact, country_id)
 	VALUES ();
 commit;
 
@@ -23,7 +23,7 @@ commit;
 /*==============================================================*/
 /* Table: SRC_Building                                          */
 /*==============================================================*/
-INSERT INTO SRC_Building (building_id, building_name, building_address, building_status, building_type, city_id)
+INSERT INTO "SSP_RES".SRC_Building (building_id, building_name, building_address, building_status, building_type, city_id)
 	VALUES ();
 commit;
 
@@ -31,7 +31,7 @@ commit;
 /*==============================================================*/
 /* Table: SRC_Apartment_Type                                    */
 /*==============================================================*/
-INSERT INTO SRC_Apartment_Type (apartment_type_id, apartment_type_name, apartment_type_desc) 
+INSERT INTO "SSP_RES".SRC_Apartment_Type (apartment_type_id, apartment_type_name, apartment_type_desc) 
 	VALUES ();
 commit;
 
@@ -39,15 +39,21 @@ commit;
 /*==============================================================*/
 /* Table: SRC_Apartment				                */
 /*==============================================================*/
-INSERT INTO SRC_Apartment (apartment_id, apartment_status, apartment_name, apartment_number, building_id, apartment_type_id)
-	VALUES ();
+INSERT INTO "SSP_RES".SRC_Apartment (apartment_id, apartment_status, apartment_name, apartment_number, building_id, apartment_type_id)
+	VALUES (1, 'Open', 'Studio with Patio', 1, 1, 1);
+INSERT INTO "SSP_RES".SRC_Apartment (apartment_id, apartment_status, apartment_name, apartment_number, building_id, apartment_type_id)
+	VALUES (2, 'Open', 'Studio with Patio and Sea View', 2, 1, 1);
+INSERT INTO "SSP_RES".SRC_Apartment (apartment_id, apartment_status, apartment_name, apartment_number, building_id, apartment_type_id)
+	VALUES (3, 'Open', 'One-Bedroom Apartment with Balcony and Sea View', 3, 1, 1);
+INSERT INTO "SSP_RES".SRC_Apartment (apartment_id, apartment_status, apartment_name, apartment_number, building_id, apartment_type_id)
+	VALUES (4, 'Open', 'Studio with Patio', 4, 1, 1);
 commit;
 
 
 /*==============================================================*/
 /* Table: SRC_Reservation 	            	                */
 /*==============================================================*/
-INSERT INTO SRC_Reservation (reserviation_id, booked, start_date, end_date, status, price, number_of_guests, payment_method, advertiser, guest_id, apartment_id)
+INSERT INTO "SSP_RES".SRC_Reservation (reserviation_id, booked, start_date, end_date, status, price, number_of_guests, payment_method, advertiser, guest_id, apartment_id)
 	VALUES ();
 commit;
 
@@ -55,7 +61,7 @@ commit;
 /*==============================================================*/
 /* Table: SRC_Content 	            	                        */
 /*==============================================================*/
-INSERT INTO SRC_Content (content_id, content_name, content_quantity)
+INSERT INTO "SSP_RES".SRC_Content (content_id, content_name, content_quantity)
 	VALUES ();
 commit;
 
@@ -63,14 +69,14 @@ commit;
 /*==============================================================*/
 /* Table: SRC_Contains 	            	                        */
 /*==============================================================*/
-INSERT INTO SRC_Contains (content_id, apartment_id) VALUES ();
+INSERT INTO "SSP_RES".SRC_Contains (content_id, apartment_id) VALUES ();
 commit;
 
 
 /*==============================================================*/
 /* Table: SRC_Apartment_Ocupancy                                */
 /*==============================================================*/
-INSERT INTO SRC_Apartment_Ocupancy (ocupancy_id, start_date, end_date, apartment_id)
+INSERT INTO "SSP_RES".SRC_Apartment_Ocupancy (ocupancy_id, start_date, end_date, apartment_id)
 	VALUES ();
 commit;
 
@@ -78,6 +84,6 @@ commit;
 /*==============================================================*/
 /* Table: SRC_Pricelist 	                                */
 /*==============================================================*/
-INSERT INTO SRC_Pricelist (pricelist_id, start_date, end_date, price_per_night, discount, apartment_id)
+INSERT INTO "SSP_RES".SRC_Pricelist (pricelist_id, start_date, end_date, price_per_night, discount, apartment_id)
 	VALUES ();
 commit;
