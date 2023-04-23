9546e31ea04d032516bf94bd7d970d6a82db247b
Aleksa Milenovic <73794793+mileni98@users.noreply.github.com>
2023-04-23 16:14:59 +0200

    feat: reorganized project directory structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # data-warehouse
+
+oracle instant client: https://www.youtube.com/watch?v=v0TkfVFGO5c&ab_channel=LearningSoftware
+
+connect sql developer to python: https://www.youtube.com/watch?v=rhwPfc8NdcQ&ab_channel=LearningSoftware
+
+ Note: when inserting data into sql developer directly, close it and open again to have that data in visual studio
--- Relational databases/SRCBoking_CreateDB.sql
+++ Relational databases/SRCBoking_CreateDB.sql
@@ -1,20 +1,20 @@
-drop table SRC_Country cascade constraints;
-drop table SRC_City cascade constraints;
-drop table SRC_Guest cascade constraints;
-drop table SRC_Building cascade constraints;
-drop table SRC_Apartment_Type cascade constraints;
-drop table SRC_Apartment cascade constraints;
-drop table SRC_Reservation cascade constraints;
-drop table SRC_Content cascade constraints;
-drop table SRC_Contains cascade constraints;
-drop table SRC_Apartment_Ocupancy cascade constraints;
-drop table SRC_Pricelist cascade constraints;
+drop table "SSP_RES".SRC_Country cascade constraints;
+drop table "SSP_RES".SRC_City cascade constraints;
+drop table "SSP_RES".SRC_Guest cascade constraints;
+drop table "SSP_RES".SRC_Building cascade constraints;
+drop table "SSP_RES".SRC_Apartment_Type cascade constraints;
+drop table "SSP_RES".SRC_Apartment cascade constraints;
+drop table "SSP_RES".SRC_Reservation cascade constraints;
+drop table "SSP_RES".SRC_Content cascade constraints;
+drop table "SSP_RES".SRC_Contains cascade constraints;
+drop table "SSP_RES".SRC_Apartment_Ocupancy cascade constraints;
+drop table "SSP_RES".SRC_Pricelist cascade constraints;
 
 
 /*==============================================================*/
 /* Table: SRC_Country                                           */
 /*==============================================================*/
-CREATE TABLE SRC_Country (
+CREATE TABLE "SSP_RES".SRC_Country (
     country_id    		INTEGER 		NOT NULL,
     country_name 		VARCHAR2(30 CHAR) 	NOT NULL,
     CONSTRAINT pk_country PRIMARY KEY ( country_id )
@@ -24,7 +24,7 @@ CREATE TABLE SRC_Country (
 /*==============================================================*/
 /* Table: SRC_City                                              */
 /*==============================================================*/
-CREATE TABLE SRC_City (
+CREATE TABLE "SSP_RES".SRC_City (
     city_id     		INTEGER 		NOT NULL,
     city_name   		VARCHAR2(30 CHAR) 	NOT NULL,
     country_id  		INTEGER 		NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE SRC_City (
 /*==============================================================*/
 /* Table: SRC_Guest                                             */
 /*==============================================================*/
-CREATE TABLE SRC_Guest (
+CREATE TABLE "SSP_RES".SRC_Guest (
     guest_id         		INTEGER 		NOT NULL,
     guest_firstname  		VARCHAR2(30 CHAR) 	NOT NULL,
     guest_lastname   		VARCHAR2(30 CHAR) 	NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE SRC_Guest (
 /*==============================================================*/
 /* Table: SRC_Building                                          */
 /*==============================================================*/
-CREATE TABLE SRC_Building (
+CREATE TABLE "SSP_RES".SRC_Building (
     building_id       		INTEGER 		NOT NULL,
     building_name     		VARCHAR2(30 CHAR) 	NOT NULL,
     building_address  		VARCHAR2(30 CHAR) 	NOT NULL,
@@ -60,17 +60,17 @@ CREATE TABLE SRC_Building (
     CONSTRAINT pk_building PRIMARY KEY ( building_id )
 );
 
-ALTER TABLE SRC_Building 
+ALTER TABLE "SSP_RES".SRC_Building 
     ADD CHECK ( building_status IN ( 'Open', 'Temporary_Closed', 'Closed' ) );
 
-ALTER TABLE SRC_Building 
+ALTER TABLE "SSP_RES".SRC_Building 
     ADD CHECK ( building_type IN ( 'In_City', 'On_Beach', 'In_Mountain' ) );
 
 
 /*==============================================================*/
 /* Table: SRC_Apartment_Type                                    */
 /*==============================================================*/
-CREATE TABLE SRC_Apartment_Type (
+CREATE TABLE "SSP_RES".SRC_Apartment_Type (
     apartment_type_id    	INTEGER 		NOT NULL,
     apartment_type_name  	VARCHAR2(30 CHAR) 	NOT NULL,
     apartment_type_desc  	VARCHAR2(100 CHAR),
@@ -81,7 +81,7 @@ CREATE TABLE SRC_Apartment_Type (
 /*==============================================================*/
 /* Table: SRC_Apartment				                */
 /*==============================================================*/
-CREATE TABLE SRC_Apartment (
+CREATE TABLE "SSP_RES".SRC_Apartment (
     apartment_id       		INTEGER 		NOT NULL,
     apartment_status   		VARCHAR2(10) 		NOT NULL,
     apartment_name     		VARCHAR2(30 CHAR) 	NOT NULL,
@@ -91,14 +91,14 @@ CREATE TABLE SRC_Apartment (
     CONSTRAINT pk_apartment PRIMARY KEY ( apartment_id )
 );
 
-ALTER TABLE SRC_Apartment 
+ALTER TABLE "SSP_RES".SRC_Apartment 
     ADD CHECK ( apartment_status IN ( 'Open', 'Temporary_Closed', 'Closed' ) );
 
 
 /*==============================================================*/
 /* Table: SRC_Reservation 	            	                */
 /*==============================================================*/
-CREATE TABLE SRC_Reservation (
+CREATE TABLE "SSP_RES".SRC_Reservation (
     reserviation_id   		INTEGER 		NOT NULL,
     booked            		DATE 			NOT NULL,
     start_date        		DATE 			NOT NULL,
@@ -113,17 +113,17 @@ CREATE TABLE SRC_Reservation (
     CONSTRAINT pk_reservation PRIMARY KEY ( reserviation_id ) 
 );
 
-ALTER TABLE SRC_Reservation 
+ALTER TABLE "SSP_RES".SRC_Reservation 
     ADD CHECK ( status IN ( 'Confirmed', 'Cancelled' ) );
 
-ALTER TABLE SRC_Reservation 
+ALTER TABLE "SSP_RES".SRC_Reservation 
     ADD CHECK ( payment_method IN ( 'Card', 'Cash' ) );
 
 
 /*==============================================================*/
 /* Table: SRC_Content 	            	                        */
 /*==============================================================*/
-CREATE TABLE SRC_Content (
+CREATE TABLE "SSP_RES".SRC_Content (
     content_id        		INTEGER 		NOT NULL,
     content_name      		VARCHAR2(30 CHAR) 	NOT NULL,
     content_quantity  		INTEGER,
@@ -134,7 +134,7 @@ CREATE TABLE SRC_Content (
 /*==============================================================*/
 /* Table: SRC_Contains 	            	                        */
 /*==============================================================*/
-CREATE TABLE SRC_Contains (
+CREATE TABLE "SSP_RES".SRC_Contains (
     content_id    		INTEGER 		NOT NULL,
     apartment_id  		INTEGER 		NOT NULL,
     CONSTRAINT pk_contains PRIMARY KEY ( content_id, apartment_id )
@@ -144,7 +144,7 @@ CREATE TABLE SRC_Contains (
 /*==============================================================*/
 /* Table: SRC_Apartment_Ocupancy                                */
 /*==============================================================*/
-CREATE TABLE SRC_Apartment_Ocupancy (
+CREATE TABLE "SSP_RES".SRC_Apartment_Ocupancy (
     ocupancy_id   		INTEGER 		NOT NULL,
     start_date    		DATE 			NOT NULL,
     end_date      		DATE 			NOT NULL,
@@ -156,7 +156,7 @@ CREATE TABLE SRC_Apartment_Ocupancy (
 /*==============================================================*/
 /* Table: SRC_Pricelist 	                                */
 /*==============================================================*/
-CREATE TABLE SRC_Pricelist (
+CREATE TABLE "SSP_RES".SRC_Pricelist (
     pricelist_id     		INTEGER 		NOT NULL,
     start_date       		DATE 			NOT NULL,
     end_date         		DATE 			NOT NULL,
@@ -168,46 +168,46 @@ CREATE TABLE SRC_Pricelist (
 
 
 
-ALTER TABLE SRC_Apartment 
+ALTER TABLE "SSP_RES".SRC_Apartment 
     ADD CONSTRAINT fk_apart_apart_type FOREIGN KEY ( apartment_type_id )
-        REFERENCES SRC_Apartment_Type ( apartment_type_id );
+        REFERENCES "SSP_RES".SRC_Apartment_Type ( apartment_type_id );
 
-ALTER TABLE SRC_Apartment 
+ALTER TABLE "SSP_RES".SRC_Apartment 
     ADD CONSTRAINT fk_apartment_building FOREIGN KEY ( building_id )
-        REFERENCES SRC_Building ( building_id );
+        REFERENCES "SSP_RES".SRC_Building ( building_id );
 
-ALTER TABLE SRC_Building 
+ALTER TABLE "SSP_RES".SRC_Building 
     ADD CONSTRAINT fk_building_city FOREIGN KEY ( city_id )
-        REFERENCES SRC_City ( city_id );
+        REFERENCES "SSP_RES".SRC_City ( city_id );
 
-ALTER TABLE SRC_City 
+ALTER TABLE "SSP_RES".SRC_City 
     ADD CONSTRAINT fk_city_country FOREIGN KEY ( country_id )
-        REFERENCES SRC_Country ( country_id );
+        REFERENCES "SSP_RES".SRC_Country ( country_id );
 
-ALTER TABLE SRC_Contains 
+ALTER TABLE "SSP_RES".SRC_Contains 
     ADD CONSTRAINT fk_contains_apart FOREIGN KEY ( apartment_id )
-        REFERENCES SRC_Apartment ( apartment_id );
+        REFERENCES "SSP_RES".SRC_Apartment ( apartment_id );
 
-ALTER TABLE SRC_Contains 
+ALTER TABLE "SSP_RES".SRC_Contains 
     ADD CONSTRAINT fk_contains_content FOREIGN KEY ( content_id )
-        REFERENCES SRC_Content ( content_id );
+        REFERENCES "SSP_RES".SRC_Content ( content_id );
 
-ALTER TABLE SRC_Guest 
+ALTER TABLE "SSP_RES".SRC_Guest 
     ADD CONSTRAINT fk_guest_country FOREIGN KEY ( country_id )
-        REFERENCES SRC_Country ( country_id );
+        REFERENCES "SSP_RES".SRC_Country ( country_id );
 
-ALTER TABLE SRC_Apartment_Ocupancy 
+ALTER TABLE "SSP_RES".SRC_Apartment_Ocupancy 
     ADD CONSTRAINT fk_occupancy_apart FOREIGN KEY ( apartment_id )
-        REFERENCES SRC_Apartment ( apartment_id );
+        REFERENCES "SSP_RES".SRC_Apartment ( apartment_id );
 
-ALTER TABLE SRC_Pricelist 
+ALTER TABLE "SSP_RES".SRC_Pricelist 
     ADD CONSTRAINT fk_pricelist_apartment FOREIGN KEY ( apartment_id )
-        REFERENCES SRC_Apartment ( apartment_id );
+        REFERENCES "SSP_RES".SRC_Apartment ( apartment_id );
 
-ALTER TABLE SRC_Reservation 
+ALTER TABLE "SSP_RES".SRC_Reservation 
     ADD CONSTRAINT fk_res_apartm FOREIGN KEY ( apartment_id )
-        REFERENCES SRC_Apartment ( apartment_id );
+        REFERENCES "SSP_RES".SRC_Apartment ( apartment_id );
 
-ALTER TABLE SRC_Reservation 
+ALTER TABLE "SSP_RES".SRC_Reservation 
     ADD CONSTRAINT fk_res_guest FOREIGN KEY ( guest_id )
-        REFERENCES SRC_Guest ( guest_id );
+        REFERENCES "SSP_RES".SRC_Guest ( guest_id );
